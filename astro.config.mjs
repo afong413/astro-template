054cd8cd1894cd2a9f2efd12529ba5e07fe8dd8a
Aiden Fong <afong413@gmail.com>
2025-05-21 22:06:07 -0700

    Install packages, update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+import react from "@astrojs/react"
+import tailwindcss from "@tailwindcss/vite"
+import vercel from "@astrojs/vercel"
+import path from "path"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: vercel(),
+  output: "server",
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
+  },
+})
